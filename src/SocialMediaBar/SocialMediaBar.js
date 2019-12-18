ca419af7a94ae935d95421e73d9347bc79dfd735
Kenneth D'Aquila <kfd182@gmail.com>
2019-12-17 19:50:01 -0600

    Added Social Media Bar component

---
+++ src/SocialMediaBar/SocialMediaBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SocialMediaBar() {
+    return (
+        <div className="social-media">
+            <a className="social-media__link" href="#"><img className="social-media__logo" src="../images/f_Logo_Online_04_2019/Color/PNG/f_logo_RGB-Blue_72.png" alt="facebook logo"/></a>
+            <a className="social-media__link" href="#"><img className="social-media__logo" src="../images/Twitter_Social_Icons/Twitter_SocialIcon_RoundedSquare/Twitter_Social_Icon_Rounded_Square_Color.svg" alt="twitter logo"/></a>
+            <a className="social-media__link" href="#"><img className="social-media__logo" src="../images/Instagram_Glyph/glyph-logo_May2016.png" alt="instagram logo"/></a>
+            <a className="social-media__link" href="#"><img className="social-media__logo" src="../images/LinkedIn-Logos/LI-In-Bug.png" alt="linkedin logo"/></a>
+        </div>
+    )
+}
