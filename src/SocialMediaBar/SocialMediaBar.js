23268cf0ce9acd4c1a75969174c1bd8840837c62
Kenneth D'Aquila <kfd182@gmail.com>
2019-12-18 02:21:22 -0600

    Social media icon styles updated

--- src/SocialMediaBar/SocialMediaBar.js
+++ src/SocialMediaBar/SocialMediaBar.js
@@ -1,12 +1,17 @@
 import React from "react";
+import styles from "./SocialMediaBar.scss";
+import fbLogo from "../images/f_Logo_Online_04_2019/Color/PNG/f_logo_RGB-Blue_72.png";
+import twLogo from "../images/Twitter_Social_Icons/Twitter_SocialIcon_RoundedSquare/Twitter_Social_Icon_Rounded_Square_Color.svg";
+import igLogo from "../images/Instagram_Glyph/glyph-logo_May2016.png";
+import liLogo from "../images/LinkedIn-Logos/LI-In-Bug.png";
 
 export default function SocialMediaBar() {
     return (
         <div className="social-media">
-            <a className="social-media__link" href="#"><img className="social-media__logo" src="../images/f_Logo_Online_04_2019/Color/PNG/f_logo_RGB-Blue_72.png" alt="facebook logo"/></a>
-            <a className="social-media__link" href="#"><img className="social-media__logo" src="../images/Twitter_Social_Icons/Twitter_SocialIcon_RoundedSquare/Twitter_Social_Icon_Rounded_Square_Color.svg" alt="twitter logo"/></a>
-            <a className="social-media__link" href="#"><img className="social-media__logo" src="../images/Instagram_Glyph/glyph-logo_May2016.png" alt="instagram logo"/></a>
-            <a className="social-media__link" href="#"><img className="social-media__logo" src="../images/LinkedIn-Logos/LI-In-Bug.png" alt="linkedin logo"/></a>
+            <a className={styles.socialMedia__link} href="#"><img className={styles.socialMedia__logo} src={fbLogo} alt="facebook logo"/></a>
+            <a className={styles.socialMedia__link} href="#"><img className={styles.socialMedia__logo} src={twLogo} alt="twitter logo"/></a>
+            <a className={styles.socialMedia__link} href="#"><img className={styles.socialMedia__logo} src={igLogo} alt="instagram logo"/></a>
+            <a className={styles.socialMedia__link} href="#"><img className={styles.socialMedia__logo} src={liLogo} alt="linkedin logo"/></a>
         </div>
     )
 }
