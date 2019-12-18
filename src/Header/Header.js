8520d68b0ab82926e403914dd1ff28864ac190cb
Kenneth D'Aquila <kfd182@gmail.com>
2019-12-18 02:20:53 -0600

    Finished header content

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,6 +3,7 @@ import CallToAction from "./CallToAction";
 import NavQuick from "./NavQuick";
 import Search from "./Search";
 import styles from "./Header.scss";
+import logo from "../images/oakton_logo_header.jpg";
 
 export default function Header() {
     return (
@@ -12,6 +13,30 @@ export default function Header() {
                 <NavQuick/>
                 <Search/>
             </div>
+            <div className={styles.header__row2}>
+                <img className={styles.logo} src={logo} alt="oakton logo"/>
+                <ul className={styles.rolesList}>
+                    <li className={styles.rolesList__item}><a href="#">New Students</a></li>
+                    <li className={styles.rolesList__item}><a href="#">Current Students</a></li>
+                    <li className={styles.rolesList__item}><a href="#">Faculty & Staff</a></li>
+                </ul>
+                <ul className={styles.rolesList}>
+                    <li className={styles.rolesList__item}><a href="#">The Community</a></li>
+                    <li className={styles.rolesList__item}><a href="#">Business & Industry</a></li>
+                    <li className={styles.rolesList__item}><a href="#">Oakton Alumni</a></li>
+                </ul>
+            </div>
+            <ul className={styles.navBar}>
+                <li className={styles.navBar__item}><a href="#">About</a></li>
+                <li className={styles.navBar__item}><a href="#">Academics</a></li>
+                <li className={styles.navBar__item}><a href="#">Continuing Education</a></li>
+                <li className={styles.navBar__item}><a href="#">Admission</a></li>
+                <li className={styles.navBar__item}><a href="#">Student Life</a></li>
+                <li className={styles.navBar__item}><a href="#">Student Services</a></li>
+                <li className={styles.navBar__item}><a href="#">Library</a></li>
+                <li className={styles.navBar__item}><a href="#">News & Events</a></li>
+                <li className={styles.navBar__item}><a href="#">Giving</a></li>
+            </ul>
         </header>
     )
 }
